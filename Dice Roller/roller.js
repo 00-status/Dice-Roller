6302a2eeca53f4bd541e73c3f603f027fd1a143e
Liam Johnson <johnson1882@saskpolytech.ca>
2018-04-04 20:30:04 -0600

    Made initial GUI and JavaScript stuff.

---
+++ Dice Roller/roller.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+
+    // function to roll various sizes of dice
+    function RollDice(size) {
+        // generate a random number between 1 and the chosen size
+        var result = Math.floor(Math.random() * size) + 1;
+
+        alert(result);
+    };
+
+
+    // Click functions for dice
+    $("#dice4").click(function () { RollDice(4); });
+    $("#dice6").click(function () { RollDice(6); });
+    $("#dice8").click(function () { RollDice(8); });
+    $("#dice10").click(function () { RollDice(10); });
+    $("#dice12").click(function () { RollDice(12); });
+    $("#dice20").click(function () { RollDice(20); });
+});
